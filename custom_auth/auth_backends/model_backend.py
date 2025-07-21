15029003a08d9c6e302d2f5c4553d60dd01b0ce3
Parvez Khan Pathan <parvez.khan@silverbluewater.com>
2025-07-21 18:16:32 +0530

    [This changes are for live render]

--- custom_auth/auth_backends/model_backend.py
+++ custom_auth/auth_backends/model_backend.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
-from django.utils.translation import ugettext as _
+# from django.utils.translation import ugettext as _
+from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import PermissionDenied
 
--- fantasy_game_site/urls.py
+++ fantasy_game_site/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 from rest_framework import permissions
-from drf_yasg2.views import get_schema_view
-from drf_yasg2 import openapi
+# from drf_yasg2.views import get_schema_view
+# from drf_yasg2 import openapi
 
 
 urlpatterns = [
@@ -31,22 +31,22 @@ urlpatterns = [
 ]
 
 # Swagger scema_view & urlpatterns
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Fantasy Game API",
-        default_version='v1',
-        description="Fantasy Game API",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
-urlpatterns += [
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Fantasy Game API",
+#         default_version='v1',
+#         description="Fantasy Game API",
+#         terms_of_service="https://www.google.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@snippets.local"),
+#         license=openapi.License(name="BSD License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
+#
+# urlpatterns += [
+#     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+#     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+# ]
 
